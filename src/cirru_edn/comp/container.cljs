a76922631255407ae349debb126eb93f126f14fc
jiyinyiyong <jiyinyiyong@gmail.com>
2019-12-01 23:15:34 +0800

    some UI refinements

--- src/cirru_edn/comp/container.cljs
+++ src/cirru_edn/comp/container.cljs
@@ -21,23 +21,32 @@
    (div
     {:style (merge ui/global ui/fullscreen ui/column)}
     (div
-     {:style (merge ui/row-parted)}
-     (span nil)
-     (div
-      {:style ui/row-middle}
-      (button
-       {:style ui/button,
-        :inner-text "Parse",
-        :on-click (fn [e d! m!] (d! :result (write-edn (parse (:draft state)) {:indent 2})))})
-      (button
-       {:style ui/button,
-        :inner-text "Write",
-        :on-click (fn [e d! m!] (d! :result (write (read-string (:draft state)))))})))
+     {:style (merge ui/row-center {:padding 8})}
+     (button
+      {:style ui/button,
+       :inner-text "Parse Cirru to EDN",
+       :on-click (fn [e d! m!]
+         (try
+          (d! :result (write-edn (parse (:draft state)) {:indent 2}))
+          (catch js/Error e (d! :error e))))})
+     (=< 8 nil)
+     (button
+      {:style ui/button,
+       :inner-text "Write Cirru from EDN",
+       :on-click (fn [e d! m!]
+         (try
+          (d! :result (write (read-string (:draft state))))
+          (catch js/Error e (d! :error e))))}))
+    (div {:style ui/center} (if (some? (:error store)) (<> (:error store) {:color :red})))
     (div
      {:style (merge ui/expand ui/row)}
      (textarea
       {:style (merge ui/expand ui/textarea style-code),
        :value (:draft state),
-       :on-input (fn [e d! m!] (m! (assoc state :draft (:value e))))})
-     (textarea {:style (merge ui/expand ui/textarea style-code), :value (:result store)}))
+       :on-input (fn [e d! m!] (m! (assoc state :draft (:value e))) (d! :result "")),
+       :placeholder "Data..."})
+     (textarea
+      {:style (merge ui/expand ui/textarea style-code),
+       :value (:result store),
+       :placeholder "Compile result.."}))
     (when dev? (cursor-> :reel comp-reel states reel {})))))
